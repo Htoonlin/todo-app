90da812b10fd637e2c9001e59ef6ce2af1a179c2
Htoonlin <wizard.htoon@gmail.com>
2022-06-18 11:50:56 +0630

    Finished todo app

--- src/page/Task.js
+++ src/page/Task.js
@@ -1,30 +1,51 @@
 import "../App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import TodoApi from "../api/TodoApi";
 
 function Task() {
     const [tasks, setTasks] = useState([]);
     const [note, setNote] = useState("");
 
-    const addTask = (event) => {
-        //Doesn't refresh on submit form
-        event.preventDefault();
+    useEffect(() => {
+        loadTasks();
+    }, []);
 
-        //If note is blank, stop here!
+    const loadTasks = async () => {
+        const data = await TodoApi.getAll();
+        setTasks(data);
+    };
+
+    const addTask = async () => {
         if (note.trim().length <= 0) {
             return;
         }
+        await TodoApi.add({ note: note, completed: false });
+        await loadTasks();
 
-        //Prepend task in existing tasks array
-        setTasks([{ note: note, completed: false }, ...tasks]);
         //Clear input field
         setNote("");
+    };
+
+    const handleCompleted = async (task) => {
+        await TodoApi.update(task);
+        await loadTasks();
+    };
+
+    const removeTask = async (id) => {
+        await TodoApi.remove(id);
+        await loadTasks();
     };
 
     const renderTaskItem = (item, index) => {
         return (
             <li key={index} className="task-item">
-                <input type="checkbox" checked={item.completed} />
-                {item.note}
+                <div className="task-info" onClick={(event) => handleCompleted({ ...item, completed: !item.completed })}>
+                    <span className="material-icons task-checked">{item.completed ? "check_box" : "check_box_outline_blank"}</span>
+                    <span className={item.completed ? "task-completed" : ""}>{item.note}</span>
+                </div>
+                <button className="task-item-remove" onClick={(event) => removeTask(item.id)}>
+                    Del
+                </button>
             </li>
         );
     };
@@ -32,7 +53,13 @@ function Task() {
     return (
         <div className="container">
             <h1 className="header">Shopping List</h1>
-            <form className="form" onSubmit={addTask}>
+            <form
+                className="form"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    addTask();
+                }}
+            >
                 <input className="input" type="text" placeholder="Create new task" value={note} onChange={(event) => setNote(event.target.value)} />
                 <button type="submit" className="button">
                     Add
